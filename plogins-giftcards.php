063a77ff0593e40a74c019dd5f5a8053c8c42cc2
wppoland <szatkowski.mariusz+github@gmail.com>
2026-07-07 12:32:27 +0200

    release: 1.0.1

--- plogins-giftcards.php
+++ plogins-giftcards.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Plogins Gift Cards - Store Credit for WooCommerce
  * Plugin URI:        https://plogins.com/plogins-giftcards/
  * Description:        Sell gift cards that email a redeemable code to the recipient and apply as a discount at checkout.
- * Version:           0.2.1
+ * Version:           1.0.1
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Requires Plugins:  woocommerce
@@ -24,7 +24,7 @@ namespace GiftCards;
 
 defined('ABSPATH') || exit;
 
-const VERSION     = '0.2.1';
+const VERSION     = '1.0.1';
 const PLUGIN_FILE = __FILE__;
 
 define('GIFTCARDS_DIR', plugin_dir_path(__FILE__));
